77051004cf321be8abe6e2ccc385d5d21f9e0543
Wojciech Wais <wojciech.wais@gmail.com>
2015-11-28 17:58:25 +0100

    small changes, load other xml files

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import aiml
 
-# Create the kernel and learn AIML files
 bot = aiml.Kernel()
-#kernel.learn("speak.xml")
-#kernel.learn("random_answers.xml")A
+bot.setBotPredicate("name", "Alex")
+bot.setBotPredicate("master", "Wojtek")
+bot.learn("speak.xml")
+bot.learn("random_answers.xml")
 bot.learn("Religion.aiml")
-#kernel.respond("load aiml b")
 
-# Press CTRL-C to break this loop
+
 while True:
-    print kernel.respond(raw_input(" > "))
+    print bot.respond(raw_input(" > "))
