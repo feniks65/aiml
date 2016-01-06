d975363202193b726efe78e7a96cbeec9f55ed46
Wojciech Wais <wojciech.wais@gmail.com>
2016-01-06 22:35:12 +0100

    last changes

--- bot.py
+++ bot.py
@@ -4,9 +4,31 @@
 import aiml
 import xml.etree.cElementTree as ET
 import urllib2
+import datetime
+import re
+
+#tabela z kursami kupna i sprzedaży walut obcych
+adres_tabeli_C = "http://www.nbp.pl/kursy/xml/dir.aspx?tt=C"
+tabela_C = urllib2.urlopen(adres_tabeli_C)
+#w niej trzeba znaleźć xml z dzisiaj
+
+now = datetime.datetime.now()
+
+# zbuduj część nazwy pliku z dzisiejszymi kursami walut
+part_filename = str(now.day) + str(now.month) + str((now.year)[-2:]) + '.xml'
+
+# poszukaj regex dalszej części pliku
+matches = re.search("c[A-Za-z0-9]*" + part_filename, tabela_C.read())
+
+# dzisiejsza strona z kursami walut
+actual_exchanges = matches.group()
 
 #sciagnij plik z serwera z nbpu do pliku
-exchange_rates_site = urllib2.urlopen("http://www.nbp.pl/kursy/xml/c231z151127.xml")
+# pobierz aktualną datę, przeszukaj stronę
+# http://www.nbp.pl/kursy/xml/dir.aspx?tt=C
+# pod kątem tego pliku z aktualną datą
+# pobierz ten plik
+exchange_rates_site = urllib2.urlopen(actual_exchanges)
 exchange_rates_fd = open('exchange_rates.xml','wb')
 exchange_rates_fd.write(exchange_rates_site.read())
 exchange_rates_fd.close()
@@ -19,6 +41,12 @@ bot.setBotPredicate("name", "Alex")
 bot.setBotPredicate("master", "Wojtek")
 #bot.learn("random_answers.xml")
 #bot.learn("Religion.aiml")
+
+"""
+Potrzebuje jakiegoś xmla z najczęściej zadawanymi pytaniami i odpowiedziami po polsku i zamiast nich
+muszę go załadować.
+"""
+
 bot.learn("UI.aiml")
 bot.learn("Default.aiml")
 bot.learn("Knowledge.aiml")
