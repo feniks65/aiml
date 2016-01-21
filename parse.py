b88cbba592aa3cbfcf18d6e02b7e18a7f985267b
Wojciech Wais <wojciech.wais@gmail.com>
2016-01-21 14:06:09 +0100

    dziala dolar ameryka

--- parse.py
+++ parse.py
@@ -3,15 +3,21 @@
 
 import xml.etree.ElementTree as ET
 import sys
+import re
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-pltoang_tab = {u'ą':'a', u'ć':'c', u'ę':'e', u'ł':'l', u'ń':'n', u'ó':'o', u'ś':'s', u'ż':'z', u'ź':'z'}
 
+def changeToProperValName ( name_exch ):
+	if re.compile("dolar ameryka.*").match(name_exch):
+		return "dolar amerykanski"
+	elif re.compile("forint .*").match(name_exch):
+		return "forint"
+	elif re.compile("korona du.*").match(name_exch):
+		return "korona dunska"
+	else:
+		return name_exch
 
-
-def plToAng(text):
-    return ''.join( pltoang_tab.get(char, char) for char in text )
 
 tree = ET.parse('exchange_rates.xml')
 root = tree.getroot()
@@ -24,7 +30,7 @@ for pozycja in root.findall('pozycja'):
     kurs_kupna = pozycja.find('kurs_kupna').text
     kurs_sprzedazy = pozycja.find('kurs_sprzedazy').text
     category = ET.SubElement(aiml, "category")
-    nazw_wal = nazwa_waluty.decode('iso-8859-2').encode('utf8')
+    nazw_wal = changeToProperValName(nazwa_waluty)
     ET.SubElement(category, "pattern").text = " * " + nazw_wal.upper()
     ET.SubElement(category, "template").text = "kurs kupna " + kurs_kupna + " ; kurs sprzedaży " + kurs_sprzedazy 
 
